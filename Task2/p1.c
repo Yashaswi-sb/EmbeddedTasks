f6ab4656ba0613abc8dc4a89298e705220959140
Yashaswi-sb <145791011+Yashaswi-sb@users.noreply.github.com>
2023-10-16 08:04:26 +0530

    p1.c

--- Task2/p1.c
+++ Task2/p1.c
@@ -1,1 +1,25 @@
-
+#include <stdio.h>
+int main() {
+    int a[10],i,sume=0,sumo=0,n;
+    printf("Enter number of Array Elements:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("Enter the number %d:",i+1);
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+       if(a[i]%2==0)
+        {
+            sume=sume+a[i];
+        }
+       else
+        {
+            sumo=sumo+a[i];
+        }
+    }
+     printf("sum of even numbers=%d\n",sume);
+     printf("sum of odd numbers=%d",sumo);
+    return 0;
+}
