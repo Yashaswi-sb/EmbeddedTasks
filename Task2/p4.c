9191cc762e5ee43dcf44a99bbcc992033f14d53d
Yashaswi-sb <145791011+Yashaswi-sb@users.noreply.github.com>
2023-10-16 11:34:20 +0530

    Create p4.c

---
+++ Task2/p4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int a[15],i,n,b;
+    printf("Enter number of Array Elements:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("Enter the number %d:",i+1);
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the number to be found:");
+    scanf("%d",&b);
+    for(i=0;i<n;i++)
+    {
+        if(b==a[i])
+        printf("Number was found at index %d\n",i);
+    }
+    return 0;
+}
