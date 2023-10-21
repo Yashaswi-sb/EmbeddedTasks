caaecdb1854202fdf69f767f292562cae0763ba7
Yashaswi-sb <145791011+Yashaswi-sb@users.noreply.github.com>
2023-10-21 22:10:04 +0530

    p1.c

--- Task3/p1.c
+++ Task3/p1.c
@@ -1,1 +1,18 @@
-
+#include <string.h>
+#include <stdio.h>
+int main() {
+    char s1[30];
+    char letter;
+    int i;
+    printf("Enter a string:");
+    gets(s1);
+    printf("Enter the letter to be found:");
+    scanf("%c",&letter);
+    //printf("%c",letter);
+    for(i=0;s1[i]!='\0';i++){
+        if(s1[i]==letter){
+            printf("Index of first occurrence:%d",i);
+        }
+    }
+    return 0;
+}
