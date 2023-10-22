ca9147386f76b6bdd01d3c2ecc3a6841dcc607a5
Yashaswi-sb <145791011+Yashaswi-sb@users.noreply.github.com>
2023-10-22 11:33:24 +0530

    Update p1.c

--- Task5/p1.c
+++ Task5/p1.c
@@ -1,1 +1,19 @@
-
+#include <string.h>
+#include <stdio.h>
+int main() {
+    char s1[30],s2[30];
+    int i,l1,l2;
+    char ch;
+    printf("Enter the string 1:");
+    gets(s1);
+    printf("Enter the string 2:");
+    gets(s2);
+    l1=strlen(s1);
+    l2=strlen(s2);
+    for(i=0;i<l2;i++){
+        s1[l1+i]=s2[i];
+    }
+    printf("String 1: %s\n",s1);
+    printf("String 2: %s ",s2);
+    return 0;
+}
